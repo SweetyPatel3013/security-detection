f025ac0f2c4cf7c5e649d4b4aee31b18fbf7ce21
Milo <david.espitia@payulatam.com>
2021-07-24 10:12:28 -0500

    Changes in the frontend

--- security-web-app/app.py
+++ security-web-app/app.py
@@ -8,13 +8,20 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/faces')
+def persons():
+    return render_template('AddPerson.html')
+
+
 @app.route('/faces/recognition')
 def face_recognition():
     return render_template('FaceRecognition.html')
 
+
 @app.route('/movements')
 def movements():
     return render_template('MovementDetector.html')
+
 
 if __name__ == "__main__":
     app.run(debug=False)
