f90940df0fa7de6ae19002b2f1bd9af4abb06138
Sweety Patel <34836824+SweetyPatel3013@users.noreply.github.com>
2021-07-31 04:17:40 -0400

    Modified GUI design and still working with ADD PERSON page and mask detection page added

--- security-web-app/app.py
+++ security-web-app/app.py
@@ -5,23 +5,34 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index_new.html')
 
 
 @app.route('/faces')
 def persons():
+    # return render_template('AddPerson.html')
+    return render_template('add-person_new.html')
+
+
+@app.route('/faces1')
+def persons1():
     return render_template('AddPerson.html')
 
 
 @app.route('/faces/recognition')
 def face_recognition():
-    return render_template('FaceRecognition.html')
+    return render_template('face-recognition_new.html')
+    # return render_template('FaceRecognition.html')
 
 
 @app.route('/movements')
 def movements():
-    return render_template('MovementDetector.html')
+    return render_template('movement-detector_new.html')
+    # return render_template('MovementDetector.html')
 
+@app.route('/facemask')
+def movements():
+    return render_template('face-mask_new.html')
 
 if __name__ == "__main__":
     app.run(debug=False)
